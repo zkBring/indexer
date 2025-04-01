d01b694a54cbd24d207844d1fa817da72fadba5c
eektheeek <larionov.eek@gmail.com>
2025-04-01 18:32:28 +0300

    add webhook end-points

---
+++ src/controllers/webhook-controller.js
@@ -0,0 +1,38 @@
+const logger = require('../utils/logger')
+const { dropService } = require('../services/index')
+
+const handleDropStopWebhook = async (req, res) => {
+  logger.json({ controller: 'webhook-controller', method: 'handleDropStopWebhook' })
+  const dropAddress = req.body.drop_address 
+  await dropService.updateStatus({ dropAddress, status: 'stopped' })
+
+  res.status(200).end()
+}
+
+const handleTotalStakedWebhook = async (req, res) => {
+  logger.json({ controller: 'webhook-controller', method: 'handleTotalStakedWebhook' })
+  const {
+    drop_address: dropAddress,
+    total_staked: totalStaked
+  } = req.body
+  await dropService.updateTotalStaked({ dropAddress, totalStaked })
+
+  res.status(200).end()
+}
+
+const handleMetadataUpdateWebhook = async (req, res) => {
+  logger.json({ controller: 'webhook-controller', method: 'handleMetadataUpdateWebhook' })
+  const {
+    drop_address: dropAddress,
+    metadata_ipfs_hash: metadataIpfsHash
+  } = req.body
+  await dropService.updateTitleAndDescription({ dropAddress, metadataIpfsHash })
+
+  res.status(200).end()
+}
+
+module.exports = {
+  handleDropStopWebhook,
+  handleTotalStakedWebhook,
+  handleMetadataUpdateWebhook
+} 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -23,6 +23,24 @@ const routes = {
       method: 'uploadDropMetadata',
       celebrateSchema: 'uploadDropMetadata'
     }
+  },
+  '/webhooks/drop-stop': {
+    post: {
+      controller: 'webhook-controller',
+      method: 'handleDropStopWebhook'
+    }
+  },
+  '/webhooks/total-staked': {
+    post: {
+      controller: 'webhook-controller',
+      method: 'handleTotalStakedWebhook'
+    }
+  },
+  '/webhooks/metadata-update': {
+    post: {
+      controller: 'webhook-controller',
+      method: 'handleMetadataUpdateWebhook'
+    }
   }
 }
 
